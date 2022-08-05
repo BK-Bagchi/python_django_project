1489808b8599b8c8544487829a8a94ff0e8dd254
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-08-05 23:13:39 +0600

    Created a view

--- django_app/views.py
+++ django_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def djangoproject(request):
+    return HttpResponse('Starting a project as part of learning Django. Hope this project will be a fun.')
