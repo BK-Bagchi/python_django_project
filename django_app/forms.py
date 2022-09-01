53e1ad2050f679304ba5f131f9b50c33a931e55a
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-09-01 16:13:56 +0600

    program created for django_form

---
+++ django_app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class user_form(forms.Form):
+    user_name = forms.CharField(max_length=20, label="Full Name", widget=forms.TextInput(
+        attrs={'placeholder': 'Enter you full name', 'style': 'width: 300px'}))
+    user_email = forms.EmailField(max_length=20, label="Email", widget=forms.TextInput(
+        attrs={'placeholder': 'Enter your email'}))
+    user_birthday = forms.DateField(label="Birthday",
+                                    widget=forms.TextInput(attrs={'type': 'date'}))
